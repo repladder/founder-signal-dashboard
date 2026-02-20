3ef5f65187de080a5730f81417f371ae2369d6a5
nishilpedia <nishil@outboundgen.com>
2026-02-20 13:10:29 +0530

    Add Vercel configuration

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: false,
-      },
-    ]
-  },
-  // Remove this entire experimental block
 }
 
 module.exports = nextConfig
